ae6babf467411a96640e94223ef989d3bc66e1fe
Jakub Kuźma <qoobaa@gmail.com>
2008-10-29 20:33:22 +0100

    Minor changes

--- activerecord_examples.rb
+++ activerecord_examples.rb
@@ -1,21 +1,8 @@
-# ActiveRecord - brak XMLa
-# =====================
-# in database.yml
-development:
-  adapter: mysql
-  database: demo
-  username: admin
-  password: password
-  host: localhost
+# -*- coding: utf-8 -*-
 
-# create script
-  create table posts(
-    id int auto_increment primary key,
-    title varchar(75) )
+# ruby script/generate model post  --skip-migration #
 
- ruby script/generate model post  --skip-migration #
-
- # plik Post.rb
+# plik Post.rb
 class Post < ActiveRecord::Base
 end
 
@@ -24,57 +11,22 @@ Post.create(:title=>"SRUG")
 
 # ActiveRecord - Migracje
 # =====================
-ruby script/generate migration add_content_to_posts content:string category_id:integer # sprawdzic !!!
+# ruby script/generate migration add_content_to_posts content:string category_id:integer # sprawdzic !!!
 
-class AddContentToPosts < ActiveRecord::Migration
-  def self.up
-    add_column :posts, :content, :string
-    add_column :posts, :category_id, :integer
-  end
+# rake db:migrate #
 
-  def self.down
-    remove_column :posts, :content
-    remove_column :posts, :category_id
-  end
-end
-
-rake db:migrate #
-
-rake db:migrate VESION=0
+# rake db:migrate VERSION=0
 
 # ActiveRecord - Asocjacje
 # =====================
-class Post < ActiveRecord::Base
-  belongs_to :category
-  has_many :comments
-end
-
-class Category < ActiveRecord::Base
-  has_many :posts
-end
-
-class Comment < ActiveRecord::Base
-  belongs_to :post
-end
-
 post = Post.find 15
-post.comments.find_all_by_content("Srug")
-post.comments.create(:content => 'cos tam')
+post.comments.find_all_by_content "Srug"
+post.comments.create :content => 'cos tam'
 post.comments.last
 post.comments.delete_all
 
 # ActiveRecord - Walidatory
 # =====================
-class Post < ActiveRecord::Base
-  validates_presence_of :title, :content
-  validates_uniqueness_of :content
-  validates_associated :category, :on => :create
-  validate :my_own_validator
-
-  def my_own_validator
-    errors.add(:title, "Nie podoba mi się") unless title.nice_and_short?
-  end
-end
 
 p = Post.create
 p.errors.to_a
--- openclasses1.rb
+++ openclasses1.rb
@@ -2,11 +2,8 @@ class Array
   def shuffle
     sort_by { rand }
   end
-  def shuffle!
-    self.replace shuffle
-  end
 end
 
 a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-a.shuffle!
+a.shuffle
 #=> [2, 9, 4, 5, 1, 7, 8, 3, 6]
